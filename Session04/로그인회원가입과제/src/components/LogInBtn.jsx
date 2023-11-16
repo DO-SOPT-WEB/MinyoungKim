94b82717a189476c5bd0001ae86d04e4022bbf41
Rose <120544840+Rose-my@users.noreply.github.com>
2023-11-16 19:00:16 +0900

    필요없는 주석 삭제

--- Session04/로그인회원가입과제/src/components/LogInBtn.jsx
+++ Session04/로그인회원가입과제/src/components/LogInBtn.jsx
@@ -1,8 +1,5 @@
 import styled from "styled-components";
 
-// - [ ] 아이디와 비밀번호 입력후 로그인 버튼을 눌렀을시 성공하면 `/mypage/:userId` 로 넘어갑니다. 
-// (여기서 `userId`는 로그인 성공시 반환 받은 사용자의 id)
-
 const LogInBtn = (btnProps) => {
   const {logIn} = btnProps;
 
@@ -32,11 +29,3 @@ const LogInBtnWrapper = styled.button`
     border: 2px solid black;
   }
 `
-// const SignUpBtnInactivated = styled.div`
-//   border-radius: 0;
-
-//   padding: 10px 15px;
-  
-//   background-color: brown;
-//   opacity: 70%;
-// `
