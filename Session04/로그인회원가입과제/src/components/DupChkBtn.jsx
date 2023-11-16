4e5919e52cacff4a088e553ebbcb7f76f5ab6295
Rose <120544840+Rose-my@users.noreply.github.com>
2023-11-17 02:07:55 +0900

    불필요한 주석 삭제

--- Session04/로그인회원가입과제/src/components/DupChkBtn.jsx
+++ Session04/로그인회원가입과제/src/components/DupChkBtn.jsx
@@ -1,8 +1,5 @@
 import styled from "styled-components";
 import { useState } from "react";
-// - [x] ID 중복체크를 하지 않은 경우 검정색입니다.
-// - [ ] ID 중복체크 결과 중복인 경우 빨간색입니다.
-// - [ ] ID 중복체크 결과 중복이 아닌 경우 초록색입니다. 
 
 const DupChkBtn = (btnProps) => {
   const {dupCheck, isChecked} = btnProps;
@@ -27,3 +24,4 @@ const DupChkBtnWrapper = styled.button`
 
   background-color: ${props => (props.isChecked === null)? "black" : ((props.isChecked ===false?  "green" : "red"))}
 `
+
