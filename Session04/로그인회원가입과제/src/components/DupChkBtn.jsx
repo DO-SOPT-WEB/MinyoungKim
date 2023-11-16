1fd7506f2e66a82189660000216bf33b8024eff1
Rose <120544840+Rose-my@users.noreply.github.com>
2023-11-16 17:38:12 +0900

    중복 확인 : 서버의 response에 따른 색깔변화

--- Session04/로그인회원가입과제/src/components/DupChkBtn.jsx
+++ Session04/로그인회원가입과제/src/components/DupChkBtn.jsx
@@ -1,31 +1,19 @@
 import styled from "styled-components";
-import { useNavigate } from 'react-router-dom';
-
+import { useState } from "react";
 // - [x] ID 중복체크를 하지 않은 경우 검정색입니다.
 // - [ ] ID 중복체크 결과 중복인 경우 빨간색입니다.
 // - [ ] ID 중복체크 결과 중복이 아닌 경우 초록색입니다. 
 
 const DupChkBtn = (btnProps) => {
-  const {isChecked, isExist} = btnProps;
-  const navigate = useNavigate();
+  const {dupCheck, isChecked} = btnProps;
 
-  const handleClickButton = () => {
-    navigate('/login');
-  }
-  return isChecked ? (
-    isExist ? (
-      <DupChkBtnWrapper type="button" onClick={handleClickButton}>
-        회원가입
-      </DupChkBtnWrapper>
-    ) : (
-      <DupChkBtnWrapper type="button" onClick={handleClickButton}>
-        회원가입
-      </DupChkBtnWrapper>
-    )
-  ) : (
-    <DupChkBtnInactivated>중복 확인</DupChkBtnInactivated>
-  );
-  
+  return ( 
+      <DupChkBtnWrapper type="button" isChecked={isChecked} onClick={()=>{
+        dupCheck(); 
+      }}>
+        중복 확인
+      </DupChkBtnWrapper> 
+  )
 };
 
 export default DupChkBtn;
@@ -33,18 +21,9 @@ export default DupChkBtn;
 const DupChkBtnWrapper = styled.button`
   background-color: black;
   color: white;
-
+  width: 15vw;
   padding: 10px 15px;
   border-radius: 0;
 
-  background-color: ${props => (props.isEqual ? 'red' : 'green')};
-`
-const DupChkBtnInactivated = styled.div`
-  border-radius: 0;
-
-  padding: 10px 15px;
-  width:9vw;
-
-  background-color: black;
-  color: white; 
+  background-color: ${props => (props.isChecked === null)? "black" : ((props.isChecked ===false?  "green" : "red"))}
 `
