747836d4f36a7afcae9fcf2d4a286abc58d097c9
Rose <120544840+Rose-my@users.noreply.github.com>
2023-11-16 17:43:13 +0900

    라우터 path 수정

--- Session04/로그인회원가입과제/src/components/Router.jsx
+++ Session04/로그인회원가입과제/src/components/Router.jsx
@@ -2,8 +2,8 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import SignupPage from "../pages/SignupPage";
+
 import MyPage from "../pages/MyPage";
-import MyPageDetail from "../pages/MyPageDetail.jsx";
 
 const Router = () => {
   return (
@@ -12,7 +12,7 @@ const Router = () => {
         <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/mypage/:userId" element={<MyPage />}/>
+        <Route path="/mypage/:userId" element={<MyPage/>}/>
       </Routes>
     </BrowserRouter>
   );
