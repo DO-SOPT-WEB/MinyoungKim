832b27bb133d507fd6972ca4b7852dd4ff89380c
Rose <120544840+Rose-my@users.noreply.github.com>
2023-11-16 18:41:18 +0900

    필요없는 주석 삭제

--- Session04/로그인회원가입과제/src/components/SignUpBtn.jsx
+++ Session04/로그인회원가입과제/src/components/SignUpBtn.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
 import { useNavigate } from 'react-router-dom';
-//1. ID, 비밀번호, 닉네임 중 비어있는 input이 있는 경우
-//2. 중복체크를 하지 않은 경우
-//3. 중복체크의 결과가 중복인 경우
-            {/* <button type="button" disabled={!btnState} onClick={async()=>{
-              signUp();
-              handleClickButton();
-            }}>회원가입</button> */}
 
 const SignUpBtn = (btnProps) => {
   const {id, password, nickname, signUp,isChecked} = btnProps;
